0a04f8dfab4023b71aad12a3a12876fe3e26c1bf
Ashish Gaur <ashishgaur19981966@gmail.com>
2022-11-16 08:41:43 +0530

    CRUD app

---
+++ as.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+class stu(BaseModel):
+    name:str
+    roll:int   
+
+my_d = {
+    1: {"name": "ashish",
+        "roll": 1},
+    2: {"name": "Shubha",
+        "roll": 2},
+    3: {"name": "Bella",
+        "roll": 3},
+    4: {"name": "Tim",
+        "roll": 4},
+    }
+
+
+
+@app.get("/")
+async def show_all():
+    return my_d
+
+@app.post("/create/{item_id}")
+def create_item(item_id, stu: stu):
+    item_id = int(item_id)
+    stu.roll = int(stu.roll) 
+    my_d[item_id] = {"name":stu.name,"roll":stu.roll}
+    return my_d[item_id]
+
+@app.get("read/{item_id}")
+def read_item(item_id):
+    item_id = int(item_id)
+    
+    return my_d[item_id]
+
+
+@app.put("/update/{item_id}")
+def update_item(item_id, stu: stu):
+    item_id = int(item_id)
+    my_d[item_id] = {}
+    my_d[item_id] = {"name":stu.name,"roll":stu.roll}
+    return my_d[item_id]
+
+@app.delete("/delete/{item_id}")
+def delete_item(item_id):
+    item_id = int(item_id)
+    del my_d[item_id]
